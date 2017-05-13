0748f63e02fa06100603f95292113bb7cc92c52d
Michael Strüder <mikezter@gmail.com>
2017-05-13 23:56:55 +0200

    convert to table

--- weekdays/english_test.go
+++ weekdays/english_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestPivot(t *testing.T) {
 	y, m, d := 2017, time.Month(12), 30
-	date := time.Date(y, m, d, 15, 53, 12, 123445, time.UTC)
+	date := time.Date(y, m, d, 15, 53, 12, 123445, time.Local)
 	expected := time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
 	actual := pivot(date)
 	if actual != expected {
@@ -16,37 +16,35 @@ func TestPivot(t *testing.T) {
 }
 
 func TestNextMonday(t *testing.T) {
-
-	var input, expected, actual time.Time
-
-	input = newDate(2017, 5, 7) // sunday
-	actual = nextMonday(input)
-	expected = newDate(2017, 5, 8)
-	if expected != actual {
-		t.Error(input, actual, expected)
+	dates := []struct{ input, actual, expected time.Time }{
+		{
+			input:    newDate(2017, 5, 7), // sunday
+			actual:   nextMonday(newDate(2017, 5, 7)),
+			expected: newDate(2017, 5, 8),
+		}, {
+			input:    newDate(2017, 5, 6), // saturday
+			actual:   nextMonday(newDate(2017, 5, 6)),
+			expected: newDate(2017, 5, 8),
+		}, {}, {
+			input:    newDate(2017, 5, 8), // monday
+			actual:   nextMonday(newDate(2017, 5, 8)),
+			expected: newDate(2017, 5, 15),
+		}, {
+			input:    newDate(2017, 5, 9), // tuesday
+			actual:   nextMonday(newDate(2017, 5, 9)),
+			expected: newDate(2017, 5, 15),
+		}, {
+			input:    newDate(2017, 5, 13), // saturday
+			actual:   nextMonday(newDate(2017, 5, 13)),
+			expected: newDate(2017, 5, 15),
+		},
 	}
 
-	input = newDate(2017, 5, 8) // monday
-	actual = nextMonday(input)
-	expected = newDate(2017, 5, 15)
-	if expected != actual {
-		t.Error(input, actual, expected)
+	for _, c := range dates {
+		if c.expected != c.actual {
+			t.Error(c.input, c.actual, c.expected)
+		}
 	}
-
-	input = newDate(2017, 5, 9) // tuesday
-	actual = nextMonday(input)
-	expected = newDate(2017, 5, 15)
-	if expected != actual {
-		t.Error(input, actual, expected)
-	}
-
-	input = newDate(2017, 5, 13) // saturday
-	actual = nextMonday(input)
-	expected = newDate(2017, 5, 15)
-	if expected != actual {
-		t.Error(input, actual, expected)
-	}
-
 }
 
 func newDate(y, m, d int) time.Time {
